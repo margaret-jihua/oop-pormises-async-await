ed4967f1243e67eb448a74c1a169482adce7c7ee
margaret-jihua <margaret.jihua@gmail.com>
2020-07-23 14:48:45 -0700

    [Constructor function] Add constructor functions

--- app.js
+++ app.js
@@ -38,3 +38,36 @@ console.log(cat);
 console.log(catTwo);
 console.log(catTwo.longfur);
 console.log(catTwo.color);
+
+// Constructor function
+function User(name) {
+    this.name = name
+    // return this
+}
+
+let adam = new User('Adam')
+let pete = new User('Pete')
+console.log(adam.name);
+console.log(pete.name);
+
+
+
+function player(name, team, threePointShooter){
+    //this = {}
+    this.name = name
+    this.team = team
+    this.threePointShooter = threePointShooter
+    //return this
+}
+
+let Dave = new player("Dave", "Warriors", true)
+console.log(Dave);
+
+function menu(appitize, mainDish, dessert){
+    this.appitize = appitize
+    this.mainDish = mainDish
+    this.dessert = dessert
+}
+
+let lunch = new menu ('sushi','beef','cake')
+console.log(lunch);
